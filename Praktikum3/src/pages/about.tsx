02147af4076cb5903c237c5ff055c85931ce016b
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-02-18 08:32:47 +0700

    Praktikum 2 - routing dasar

--- Praktikum3/src/pages/about.tsx
+++ Praktikum3/src/pages/about.tsx
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <div style={{ padding: '20px', fontFamily: 'times new roman, sans-serif' }}>
       <h1>Tentang Saya</h1>
-      <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '20px', borderRadius: '8px' }}>
+     
         <p>
           Ini adalah halaman about Cindy
         </p>
-      </div>
+    
       <br />
       <Link href="/" style={{ color: 'blue', textDecoration: 'underline', fontSize: '16px' }}>
         Kembali ke Halaman Utama
