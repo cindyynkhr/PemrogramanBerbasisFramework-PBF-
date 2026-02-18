4353da606c5e8a032f243b4d730f7fc90176684f
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-02-18 08:32:25 +0700

    Praktikum 2 - Perbaikan dan routing dasar

--- Praktikum3/src/pages/about.tsx
+++ Praktikum3/src/pages/about.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function About() {
-  const studentInfo = {
-    nama: 'Cindy Nur Khoiriyah',
-    nim: '2341720058',
-    programStudi: 'D-4 Teknik Informatika'
-  };
-
   return (
     <div style={{ padding: '20px', fontFamily: 'times new roman, sans-serif' }}>
       <h1>Tentang Saya</h1>
       <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '20px', borderRadius: '8px' }}>
         <p>
-          <strong>Nama Mahasiswa:</strong> {studentInfo.nama}
-        </p>
-        <p>
-          <strong>NIM:</strong> {studentInfo.nim}
-        </p>
-        <p>
-          <strong>Program Studi:</strong> {studentInfo.programStudi}
+          Ini adalah halaman about Cindy
         </p>
       </div>
       <br />
