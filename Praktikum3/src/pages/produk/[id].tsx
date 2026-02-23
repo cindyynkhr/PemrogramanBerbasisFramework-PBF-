31e8e4675b2a33812faa1f0ec19dd2b9ec49b162
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-02-24 06:09:47 +0700

    Praktikum 3 - membuat halaman produk

--- Praktikum3/src/pages/produk/[id].tsx
+++ Praktikum3/src/pages/produk/[id].tsx
@@ -1,11 +1,17 @@
 import { useRouter } from "next/router";
 
 const HalamanProduk = () => {
-    //const router = useRouter();
+    const router = useRouter();
+    const { id } = router.query;
+
+    if (!router.isReady) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <h1>Halaman Produk</h1>
-            <p>Produk: </p>
+            <p>Produk: <strong>{id}</strong></p>
         </div>
     );
 };
