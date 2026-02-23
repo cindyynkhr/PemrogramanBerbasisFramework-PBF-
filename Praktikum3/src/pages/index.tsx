8b841c1b935423047f9262bf05d97c6079bfcd63
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-02-24 06:36:54 +0700

    Praktikum 3 - membuat komponen navbar final

--- Praktikum3/src/pages/index.tsx
+++ Praktikum3/src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/layouts/navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,9 +10,8 @@ export default function Home() {
   return (
     //Langkah 5 - Modifikasi Halaman Utama
     <div>
-      <Navbar />
-        <h1>Praktikum Next.js Pages Router Cindy</h1>
-        <p>Mahasiswa d4 Pengembangan Web</p>
+      <h1>Praktikum Next.js Pages Router Cindy</h1>
+      <p>Mahasiswa d4 Pengembangan Web</p>
     </div>
   )
 }
