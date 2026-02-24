2a6e42c519e2c1ce7541f8b5ae06d22ccba3110d
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-02-24 07:17:32 +0700

    Praktikum 3 - Tugas Praktikum 2

---
+++ Praktikum3/src/pages/blog/index.tsx
@@ -0,0 +1,10 @@
+const BlogIndex = () => {
+  return (
+    <div>
+      <h1>Selamat Datang di Blog Cindyy</h1>
+      <p>Ini adalah halaman blog Saya</p>
+    </div>
+  );
+};
+
+export default BlogIndex;
