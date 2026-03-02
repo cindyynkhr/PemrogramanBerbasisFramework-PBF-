0517a586ebc2dcbe0833427230a182ae9e649889
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 05:01:31 +0700

    Praktikum 6 - Membuat Custom Eror Page

---
+++ Praktikum6/src/pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+    return (
+        <div>
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf Halaman yang Anda Cari tidak Ada</p>
+        </div>
+    );
+};  
+export default Custom404;
