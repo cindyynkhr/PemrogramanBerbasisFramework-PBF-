b5f6a78136b7e1016ed3861e9e4b11eacdb06ea2
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 05:16:02 +0700

    Praktikum 6 - Styling Halaman 404

--- Praktikum6/src/pages/404.tsx
+++ Praktikum6/src/pages/404.tsx
@@ -1,6 +1,8 @@
+import styles from "@/styles/404.module.scss";
+
 const Custom404 = () => {
     return (
-        <div>
+        <div className={styles.error}>
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf Halaman yang Anda Cari tidak Ada</p>
         </div>
