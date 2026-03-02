f185977ce3c461917eebe607c9218a09d49f9385
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 03:22:43 +0700

    Praktikum 5 - Tugas 1

---
+++ Praktikum5/src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import HalamanRegister from './register';
+
+const HalamanRegisterPage = () => {
+    return (
+        <div>
+            <HalamanRegister />
+        </div>
+    );
+};
+export default HalamanRegisterPage;
--- Praktikum5/src/pages/auth/register/index.tsx
+++ Praktikum5/src/pages/auth/register/index.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from './register.module.scss';
 
 const halamanRegister = () => {
     return (
-        <div>
-            <h1>Halaman Register</h1>
+        <div className={styles.login}>
+             <h1 className="text-3xl font-bold" style={{color: '#fff'}}>Halaman Register Cindy</h1>
             <Link href="/auth/login">Ke Halaman Login</Link>
         </div>
     );
