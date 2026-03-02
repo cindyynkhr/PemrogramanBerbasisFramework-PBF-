0d6ce3ed650b3cb3747d45c13cadeb21dfa5978a
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 05:29:51 +0700

    Praktikum 6 - Tugas 1

--- Praktikum6/src/pages/404.tsx
+++ Praktikum6/src/pages/404.tsx
@@ -4,8 +4,9 @@ const Custom404 = () => {
     return (
         <div className={styles.error}>
             <img src="/not-found.png" alt="404" className={styles.error_image} />
-            <h1>404 - Halaman Tidak Ditemukan</h1>
-            <p>Maaf Halaman yang Anda Cari tidak Ada</p>
+            <h1>404</h1>
+            <h2>Halaman Tidak Ditemukan</h2>
+            <p>Maaf Halaman yang Anda Cari Tidak Ada</p>
         </div>
     );
 };  
