c62e55f2b63f53f93b19d23cc18e1d7ec82d965f
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 05:22:34 +0700

    Praktikum 6 - Menampilkan Gambar dari Folder Public

--- Praktikum6/src/pages/404.tsx
+++ Praktikum6/src/pages/404.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/404.module.scss";
 const Custom404 = () => {
     return (
         <div className={styles.error}>
+            <img src="/not-found.png" alt="404" className={styles.error_image} />
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf Halaman yang Anda Cari tidak Ada</p>
         </div>
