5655ea0dae1942133e704a03d968567d521cd0ba
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 20:41:00 +0700

    Praktikum9

---
+++ Praktikum9/src/pages/views/product/index.tsx
@@ -0,0 +1,47 @@
+import styles from "../../produk/product.module.scss";
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;  
+    image: string;
+    category: string;
+};
+
+const TampilanProduk = ({products}: {products: ProductType[]}) => {
+    return (
+        <div className={styles.produk}>
+            <h1 className={styles.produk__title}>Daftar Produk</h1>
+            <div className={styles.produk__content}>
+                {products.length === 0 ? (
+                    <div className={styles.produk__content__skeleton}>
+                        <div className={styles.produk__content__skeleton__image}></div>
+                        <div className={styles.produk__content__skeleton__name}></div>
+                        <div className={styles.produk__content__skeleton__category}></div>
+                        <div className={styles.produk__content__skeleton__price}></div>
+                    </div>
+                ) : (
+                    <>
+                    {products.map((product: ProductType) => (
+                        <div key={product.id} className={styles.produk__content__item}>
+                            <div className={styles.produk__content__item__image}>
+                                <img src={product.image} alt={product.name} className={styles.produk__content__item__image} />
+                            </div>
+                            <h4 className={styles.produk__content__item__name}>
+                                {product.name}
+                            </h4>
+                            <p className={styles.produk__content__item__category}>
+                                {product.category}
+                            </p>
+                            <p className={styles.produk__content__item__price}>
+                                Rp {product.price.toLocaleString()}
+                            </p>
+                        </div>
+                    ))}
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default TampilanProduk;
