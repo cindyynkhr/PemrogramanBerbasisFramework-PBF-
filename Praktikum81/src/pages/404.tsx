27ef26bb42d6718994f7f4a30a0ed3eb26e640dd
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 17:35:33 +0700

    Praktikum 8

---
+++ Praktikum81/src/pages/404.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "@/styles/404.module.scss";
+
+const Custom404 = () => {
+    return (
+        <div className={styles.error}>
+            <img src="/not-found.png" alt="404" className={styles.error_image} />
+            <h1>404</h1>
+            <h2>Halaman Tidak Ditemukan</h2>
+            <p>Maaf Halaman yang Anda Cari Tidak Ada</p>
+            <Link href="/" className={styles.backButton}>
+                Kembali ke Home
+            </Link>
+        </div>
+    );
+};  
+export default Custom404;
