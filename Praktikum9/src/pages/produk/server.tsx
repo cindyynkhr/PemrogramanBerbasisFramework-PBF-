b49636f0b4441ffcc3e536f0d094e7e59aea017f
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 20:49:32 +0700

    Praktikum9 -Setup Halaman SSR

---
+++ Praktikum9/src/pages/produk/server.tsx
@@ -0,0 +1,12 @@
+import TampilanProduk from "../views/product";
+
+const halamanProdukServer = () => {
+    return (
+        <div>
+            <h1>Halaman Produk Server by Cindy</h1>
+            <TampilanProduk products={[]}/>
+        </div>
+    );
+};
+
+export default halamanProdukServer;
