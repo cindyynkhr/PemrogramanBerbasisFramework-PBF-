1c0f07064777e8b4b4e9aca15f5124b32202b393
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-03 09:15:28 +0700

    Praktikum 7 - Install Firebase

---
+++ Praktikum7/src/pages/utlis/db/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAvf-jS3nKiTYzI14MSrZC6wM9WY_JVbI8",
+  authDomain: "framework-next-b9cc3.firebaseapp.com",
+  projectId: "framework-next-b9cc3",
+  storageBucket: "framework-next-b9cc3.firebasestorage.app",
+  messagingSenderId: "114330171121",
+  appId: "1:114330171121:web:cce350ae52df902d499b50",
+  measurementId: "G-6NQ9EBK43F"
+};
