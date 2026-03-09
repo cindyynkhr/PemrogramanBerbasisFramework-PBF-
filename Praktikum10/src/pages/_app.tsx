6f9d15bd7e8d10e2925b69ed616e96c91521e528
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-09 15:50:48 +0700

    Praktikum10

---
+++ Praktikum10/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Appshell from '@/components/layouts/Appshell';
+import Navbar from '@/components/layouts/navbar';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Appshell>
+      <Component {...pageProps} />
+    </Appshell>
+  );
+}
+
