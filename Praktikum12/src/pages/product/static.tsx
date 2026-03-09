d451d9dd9427f53665b1bd525ac3413cd7904028
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-10 01:10:14 +0700

    Praktikum12-Perbaikan revalidate

--- Praktikum12/src/pages/product/static.tsx
+++ Praktikum12/src/pages/product/static.tsx
@@ -1,26 +1,27 @@
 import TampilanProduk from "../../views/product";
 import { ProductType } from "../../types/product.type";
 
-const halamanProdukStatic = (props:({products: ProductType[]})) => {
-    const {products} = props;
-    return(
+const halamanProdukStatic = (props: { products: ProductType[] }) => {
+    const { products } = props;
+
+    return (
         <div>
             <h1>Halaman Produk Static</h1>
             <TampilanProduk products={products} />
         </div>
-    )
+    );
 }
 
 export default halamanProdukStatic;
 
-export async function getServerSideProps() {
-    const res = await fetch('https://127.0.0.1:3000/api/produk');
-    //const responese: ProducType[] = await res.json();
+export async function getStaticProps() {
+    const res = await fetch('http://127.0.0.1:3000/api/product');
     const response: { data: ProductType[] } = await res.json();
+
     return {
         props: {
             products: response.data,
         },
-        revalidate: 10, // Revalidate every 10 seconds
+        revalidate: 10
     };
 }
