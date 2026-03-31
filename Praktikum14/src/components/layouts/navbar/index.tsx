8035bc9556b1ec6067254ec2886ea9fb665d23c2
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-03-31 18:51:08 +0700

    Praktikum14-Tambahkan Tombol Login dan Logout

--- Praktikum14/src/components/layouts/navbar/index.tsx
+++ Praktikum14/src/components/layouts/navbar/index.tsx
@@ -1,10 +1,20 @@
 import syles from "./navbar.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const Navbar = () => {
+    const {data} = useSession();
+    //const { data: sesion } = useSession();
+    //console.log("session", sesion);
     return (
         <div className={syles.navbar}>
             <div className="big">
                 Navbar
             </div>
+            {data ? (
+                <button onClick={() => signOut()}>Sign Out</button>
+            ) : (
+                <button onClick={() => signIn()}>Sign In</button>
+            )}
         </div>
     );
 };
