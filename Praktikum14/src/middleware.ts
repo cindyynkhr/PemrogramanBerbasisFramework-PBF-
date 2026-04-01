11dcacb877a7927b60240c7a1f4ab324d43a8158
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-04-01 08:13:07 +0700

    Praktikum14-Perbaikan Membuat Middleware Authorization

--- Praktikum14/src/middleware.ts
+++ Praktikum14/src/middleware.ts
@@ -1,18 +1,36 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+// export async function middleware(request: NextRequest) {
+//     const token = await getToken({
+//         req: request,
+//         secret: process.env.NEXTAUTH_SECRET,
+//     });
+
+//     if (token) {
+//         return NextResponse.next();
+//     } else {
+//         return NextResponse.redirect(new URL("/auth/login", request.url));
+//     }
+// }
+
+// export const config = {
+//     matcher: ["/product", "/about", "/profile", "/profile/:path*"],
+// };
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const isLogin = false;
-    if (isLogin) {
+    const token = request.cookies.get("next-auth.session-token");
+    if (token) {
         return NextResponse.next();
     } else {
-        return NextResponse.redirect(new URL("/auth/login", request.url));
+        return NextResponse.redirect(new URL("/", request.url));
     }
-    
-    //return NextResponse.redirect(new URL("/", request.url));
-    //return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/product", "/about"],
+    matcher: ["/product", "/about", "/profile"],
 };
