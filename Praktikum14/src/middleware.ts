e29ed215ce2b4d084f68f8d41a558bce5abff4b2
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-04-01 08:13:45 +0700

    Praktikum 14 - Menghapus Bagian Middleware yang di komen

--- Praktikum14/src/middleware.ts
+++ Praktikum14/src/middleware.ts
@@ -1,24 +1,3 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import { getToken } from "next-auth/jwt";
-
-// export async function middleware(request: NextRequest) {
-//     const token = await getToken({
-//         req: request,
-//         secret: process.env.NEXTAUTH_SECRET,
-//     });
-
-//     if (token) {
-//         return NextResponse.next();
-//     } else {
-//         return NextResponse.redirect(new URL("/auth/login", request.url));
-//     }
-// }
-
-// export const config = {
-//     matcher: ["/product", "/about", "/profile", "/profile/:path*"],
-// };
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
