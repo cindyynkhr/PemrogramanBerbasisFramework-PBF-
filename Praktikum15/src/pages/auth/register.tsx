5d33682651589b22e914e725afc4c4a686db021e
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-04-05 20:29:24 +0700

    Praktikum15-Membuat Register View

--- Praktikum15/src/pages/auth/register.tsx
+++ Praktikum15/src/pages/auth/register.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TampilanRegister from "../../views/auth/register";
 
 const halamanRegister = () => {
--- Praktikum15/src/views/auth/register/index.tsx
+++ Praktikum15/src/views/auth/register/index.tsx
@@ -1,15 +1,73 @@
 import Link from 'next/link';
-import styles from './register.module.css';
+import style from './register.module.scss';
 
-const halamanRegister = () => {
+const tampilanRegister = () => {
     return (
-        <div className={styles.register}>
-            <h1 className={styles.title}>Halaman Register</h1>
-            <p>
-                Sudah punya akun? <a href="/auth/login">Login</a>
-            </p>
+        <div className={style.register}>
+            <h1 className={style.register_title}>Halaman Register</h1>
+            <div className={style.register_form}>
+                <form action="">
+                    {/* Form Email */}
+                    <div className={style.register_form_item}>
+                        <label
+                            htmlFor="email"
+                            className={style.register_form_item_label}
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Cindy@example.com"
+                            className={style.register_form_item_input}
+                        />
+                    </div>
+
+                    {/* Form Full Name */}
+                    <div className={style.register_form_item}>
+                        <label
+                            htmlFor="fullName"
+                            className={style.register_form_item_label}
+                        >
+                            Fullname
+                        </label>
+                        <input
+                            type="text"
+                            id="fullName"
+                            name="fullName"
+                            placeholder="Cindy Nur Khoiriyah"
+                            className={style.register_form_item_input}
+                        />
+                    </div>
+
+                    {/* Form Password */}
+                    <div className={style.register_form_item}>
+                        <label
+                            htmlFor="password"
+                            className={style.register_form_item_label}
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            className={style.register_form_item_input}
+                        />
+                    </div>
+
+                    <button type="submit" className={style.register_form_item_button}>
+                        Register
+                    </button>
+                </form>
+                <p className={style.register_form_item_text}>
+                    Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+                </p>
+            </div>
         </div>
     );
 };
 
-export default halamanRegister;
+export default tampilanRegister;
