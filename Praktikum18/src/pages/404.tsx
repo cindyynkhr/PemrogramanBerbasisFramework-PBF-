b98896fcd796e2064bfa8a255084a440b8e7585a
Cindy Nur Khoiriyah <cindic455@gmail.com>
2026-04-06 16:17:25 +0700

    Pertemuan18_Optimasi gambar lokal

--- Praktikum18/src/pages/404.tsx
+++ Praktikum18/src/pages/404.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from "@/styles/404.module.scss";
@@ -10,7 +11,13 @@ const Custom404 = () => {
             </Head>
             <h1 className={styles.error__title}>404</h1>
             <p className={styles.error__desc}>Halaman yang Anda cari tidak ditemukan.</p>
-            <img src="/page-not-found.png" alt="404" className={styles.error__image} />
+                <Image
+                    src="/page-not-found.png"
+                    alt="404"
+                    width={400}
+                    height={200}
+                    className={styles.error__image}
+                />
             <p className={styles.error__info}>Silakan periksa kembali URL atau klik tombol di bawah untuk kembali ke halaman utama.</p>
             <Link href="/" className={styles.error__button}>Kembali ke Home</Link>
         </div>
